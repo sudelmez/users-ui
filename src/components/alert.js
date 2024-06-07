8391dfffe1bd5dff927573e0dd8bdf6e0ec258aa
Sude Ölmez <98457237+sudelmez@users.noreply.github.com>
2024-06-07 14:47:37 +0300

    upd

--- src/components/alert.js
+++ src/components/alert.js
@@ -2,12 +2,12 @@ import './alert.css'
 function AlertShow({ onClickedYes, onClickedNo }) {
     return (
         <div className='Alert'>
-            <h1>Are you sure you want to delete?</h1>
+            <h1 className='button'>Are you sure you want to delete?</h1>
             <div className='answer'>
-                <button onClick={onClickedYes}>
+                <button className='button' onClick={onClickedYes}>
                     <h2>Yes</h2>
                 </button>
-                <button onClick={onClickedNo}>
+                <button className='button' onClick={onClickedNo}>
                     <h2>No</h2>
                 </button>
             </div>
